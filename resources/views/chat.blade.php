847959d54c516d88c1f1897ec5683d8abacfd2a5
curder <curder@foxmail.com>
2017-12-03 14:15:42 +0800

    引用Bootstrap4并编译前端文件

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <h1>Char Room</h1>
+    </div>
+</div>
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,10 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+if (mix.inProduction) {
+    mix.version();
+}
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .extract(['vue']);
