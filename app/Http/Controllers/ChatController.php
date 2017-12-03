723f96e285e23b59e5c57f9203cab331761feebd
curder <curder@foxmail.com>
2017-12-03 15:13:22 +0800

    配置 Laravel 的事件广播系统

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\ChatEvent;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['send', 'chat']);
+    }
+
+    public function send(Request $request)
+    {
+        $message = $request->message;
+
+        $user = User::find(Auth::id());
+
+        event(new ChatEvent($message, $user));
+    }
+
+    public function chat()
+    {
+        return view('chat');
+    }
+}
